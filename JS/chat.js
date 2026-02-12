6d00d21538e6541b134b93f2ba44d0e0b06a7669
Manan <mbhongale0@gmail.com>
2026-02-12 16:51:16 -0700

    file server v6

--- JS/chat.js
+++ JS/chat.js
@@ -15,7 +15,7 @@ import {
 
 
 // GLOBAL STATE
-const UPLOAD_URL = "https://ambien-television-tex-euros.trycloudflare.com/upload";
+const UPLOAD_URL = "https://acetyl-file-server.onrender.com/upload";
 
 let messagesDiv;
 let input;
