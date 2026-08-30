7599dd73bb56aedf9b37c20cbde1f8d480b9ec88
Manan <mbhongale0@gmail.com>
2026-08-29 20:19:46 -0600

    vc v4

--- client/chat.js
+++ client/chat.js
@@ -1978,8 +1978,21 @@ const processedCandidates = {};
 const rtcConfig = {
     iceServers: [
         { urls: "stun:stun.l.google.com:19302" },
-        { urls: "stun:stun1.l.google.com:19302" },
-        { urls: "stun:stun2.l.google.com:19302" },
+        { 
+            urls: "turn:openrelay.metered.ca:80",
+            username: "openrelayproject",
+            credential: "openrelayproject"
+        },
+        { 
+            urls: "turn:openrelay.metered.ca:443",
+            username: "openrelayproject",
+            credential: "openrelayproject"
+        },
+        { 
+            urls: "turns:openrelay.metered.ca:443",
+            username: "openrelayproject",
+            credential: "openrelayproject"
+        }
     ],
     iceCandidatePoolSize: 10
 };
